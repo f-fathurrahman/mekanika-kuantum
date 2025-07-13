76ff0228e866acd337ebc2c37078bde214006c2f
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2025-07-13 15:16:53 +0700

    Setup script

--- quantum_control_jl/setup_env.jl
+++ quantum_control_jl/setup_env.jl
@@ -1,1 +1,6 @@
 Pkg.activate("quantumcontrol", shared=true)
+
+using Revise
+
+!( "./MyQuantumControl" in LOAD_PATH) && push!(LOAD_PATH, "./MyQuantumControl")
+!( "./MyQuantumPropagators" in LOAD_PATH) && push!(LOAD_PATH, "./MyQuantumPropagators")
