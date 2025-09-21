a8e33ded7b782dcdd9eb4bd12e558d436c8fc2b8
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2025-09-21 19:23:31 +0700

    heomlab spin-boson

---
+++ heomlab/debug_spin_boson_1/spin_boson_example_1.m
@@ -0,0 +1,91 @@
+clear variables; close all;
+
+% A test script for the spin boson model
+% In this example the dynamics for a spin boson model with a Debye bath are
+% calculated
+
+% Parameters for the problem
+% system hamiltonian parameters
+epsilon = 1.0 ;
+Delta = 2.0 ;
+% bath parameters
+beta = 1.0 ;
+% debye bath parameters
+lambda_D = 0.5 ;
+omega_D = 2.0 ;
+
+% dynamics information - parameters for the Short-Iterative Arnoldi
+% Integrator
+dt = 1e-2 ;
+n_steps = 1000 ;
+krylov_dim = 8 ;
+krylov_tol = 1e-8 ;
+% parmeters for heirarchy truncation using L/M truncation
+L_max = 6 ; 
+M_max = 4 ;
+
+
+% matrices of system observable operators to be returned, sigma_x, sigma_y
+% sigma_z, and 1
+O_sys = {[[0,1];[1,0]],[[0,-1.0i];[1.0i,0]],[[1,0];[0,-1]],eye(2)} ;
+
+% initial state of the system
+rho_0_sys = [[1,0];[0,0]] ;
+
+% two objects are supplied to the HEOM dynamics function:
+% "full_system" specifies the full Hamiltonian (system + bath) and the
+% temperature.
+% "heom_dynamics" specifies the HEOM truncation, integrator for the
+% dynamics and the total propagation time and observables to be calculated.
+
+% the full_system object contains all information about the Hamiltonian of
+% the full open quantum system
+full_system = struct ;
+% H_sys contains the system Hamiltonian
+full_system.H_sys = [[epsilon,Delta];
+                     [Delta,-epsilon]];
+% baths is a cell array of structs describign each bath
+full_system.baths = {struct("V",[[1,0];[0,-1]],...
+    "spectral_density","debye","omega_D",omega_D,"lambda_D",lambda_D)} ;
+full_system.beta = beta ;
+
+% a struct that contains information about the HEOM dynamics
+heom_dynamics = struct ;
+% integrator information, currently only the short iterative arnoldi is
+% implemented
+heom_dynamics.integrator = struct ;
+heom_dynamics.integrator.method = "SIA" ;
+heom_dynamics.integrator.dt = dt ;
+heom_dynamics.integrator.n_steps = n_steps ;
+heom_dynamics.integrator.krylov_dim = krylov_dim ;
+heom_dynamics.integrator.krylov_tol = krylov_tol ;
+
+% hierarchy trunction information
+heom_dynamics.heom_truncation = struct ;
+heom_dynamics.heom_truncation.truncation_method = "depth cut-off" ;
+heom_dynamics.heom_truncation.M_max = M_max ;
+heom_dynamics.heom_truncation.L_max = L_max ;
+% heom_dynamics.heom_truncation.truncation_method = "frequency cut-off" ;
+% heom_dynamics.heom_truncation.Gamma_cut = Gamma_cut ;
+heom_dynamics.heom_truncation.heom_termination = "markovian" ;
+
+% what system observables should be returned
+heom_dynamics.observables = struct ;
+heom_dynamics.observables.system = O_sys ;
+
+% set the initial condition
+heom_dynamics.rho_0_sys = rho_0_sys ;
+
+% run the dynamics
+[O_t,t] = runHEOMDynamics(full_system,heom_dynamics) ;
+
+% plot sigma_alpha(t)
+
+ylabels = {'\langle\sigma_x(\itt\rm)\rangle','\langle\sigma_y(\itt\rm)\rangle','\langle\sigma_z(\itt\rm)\rangle'};
+figure
+for i = 1:3
+subplot(3,1,i)
+plot(t,O_t(i,:))
+xlabel('\itt\rm')
+ylabel(ylabels{i})
+end
