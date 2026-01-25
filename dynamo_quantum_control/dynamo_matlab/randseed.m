21343a6958ae5562e291838e3bccfe6a0cf47053
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2026-01-25 13:25:43 +0700

    Fix random number generator

--- dynamo_quantum_control/dynamo_matlab/randseed.m
+++ dynamo_quantum_control/dynamo_matlab/randseed.m
@@ -4,4 +4,5 @@ if ~exist('seedvalue','var')
     seedvalue = sum(100*clock);
 end
 
-RandStream.setDefaultStream(RandStream('mt19937ar','seed', seedvalue));
+%RandStream.setDefaultStream(RandStream('mt19937ar','seed', seedvalue));
+RandStream.setGlobalStream(RandStream('mt19937ar', 'Seed', seedvalue));
